86e5ab834d1030a73e38e95db49f967c86eecc89
scheeloong <scheeloong@3fcf651d-5a50-4a49-f97f-91c100888062>
2012-07-09 22:28:46 +0000

    Organized all files

--- trunk/Mario/MarioApp.cpp
+++ trunk/Mario/MarioApp.cpp
@@ -1,19 +1,189 @@
-// This class is the User_Interface file. 
-// It is used to test that the Mario Game is working
+/*
 
-// Maybe the GameStates (Like StartGame, QuitGame can be handled here) 
+		Allegro 5 Shell Application v1.1
+			by Mike Geig
 
-#include "Mario.cpp"
-class MarioApp
+		This code is provided for education purposes and comes with no warantee
+		either implicit or explicit.
+
+		This code is free to use however you see fit.
+*/
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+bool keys[] = {false, false, false, false, false};
+enum KEYS{UP, DOWN, LEFT, RIGHT, SPACE};
+
+int main(int argc, char **argv)
 {
-	private:
-		Mario mario; 
-	public: 
-		// Constructor
+	//==============================================
+	//SHELL VARIABLES
+	//==============================================
+	bool done = false;
+	bool render = false;
 
-		// Main Function
-		int main(void)
+	float gameTime = 0;
+	int frames = 0;
+	int gameFPS = 0;
+
+	//==============================================
+	//PROJECT VARIABLES
+	//==============================================
+		
+
+	//==============================================
+	//ALLEGRO VARIABLES
+	//==============================================
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer;
+	ALLEGRO_FONT *font18;
+	
+	//==============================================
+	//ALLEGRO INIT FUNCTIONS
+	//==============================================
+	if(!al_init())										//initialize Allegro
+		return -1;
+
+	display = al_create_display(WIDTH, HEIGHT);			//create our display object
+
+	if(!display)										//test display object
+		return -1;
+
+	//==============================================
+	//ADDON INSTALL
+	//==============================================
+	al_install_keyboard();
+	al_init_image_addon();
+	al_init_font_addon();
+	al_init_ttf_addon();
+	al_init_primitives_addon();
+
+
+	//==============================================
+	//PROJECT INIT
+	//==============================================
+	font18 = al_load_font("arial.ttf", 18, 0);
+
+	//==============================================
+	//TIMER INIT AND STARTUP
+	//==============================================
+	event_queue = al_create_event_queue();
+	timer = al_create_timer(1.0 / 60);
+
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+	al_start_timer(timer);
+	gameTime = al_current_time();
+	while(!done)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+
+		//==============================================
+		//INPUT
+		//==============================================
+		if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
 		{
-			mario.run(); 
+			switch(ev.keyboard.keycode)
+			{
+			case ALLEGRO_KEY_ESCAPE:
+				done = true;
+				break;
+			case ALLEGRO_KEY_LEFT:
+				keys[LEFT] = true;
+				break;
+			case ALLEGRO_KEY_RIGHT:
+				keys[RIGHT] = true;
+				break;
+			case ALLEGRO_KEY_UP:
+				keys[UP] = true;
+				break;
+			case ALLEGRO_KEY_DOWN:
+				keys[DOWN] = true;
+				break;
+			case ALLEGRO_KEY_SPACE:
+				keys[SPACE] = true;
+				break;
+			}
 		}
-};
+		else if(ev.type == ALLEGRO_EVENT_KEY_UP)
+		{
+			switch(ev.keyboard.keycode)
+			{
+			case ALLEGRO_KEY_ESCAPE:
+				done = true;
+				break;
+			case ALLEGRO_KEY_LEFT:
+				keys[LEFT] = false;
+				break;
+			case ALLEGRO_KEY_RIGHT:
+				keys[RIGHT] = false;
+				break;
+			case ALLEGRO_KEY_UP:
+				keys[UP] = false;
+				break;
+			case ALLEGRO_KEY_DOWN:
+				keys[DOWN] = false;
+				break;
+			case ALLEGRO_KEY_SPACE:
+				keys[SPACE] = false;
+				break;
+			}
+		}
+		//==============================================
+		//GAME UPDATE
+		//==============================================
+		else if(ev.type == ALLEGRO_EVENT_TIMER)
+		{
+			render = true;
+
+			//UPDATE FPS===========
+			frames++;
+			if(al_current_time() - gameTime >= 1)
+			{
+				gameTime = al_current_time();
+				gameFPS = frames;
+				frames = 0;
+			}
+			//=====================
+
+		}
+
+		//==============================================
+		//RENDER
+		//==============================================
+		if(render && al_is_event_queue_empty(event_queue))
+		{
+			render = false;
+			al_draw_textf(font18, al_map_rgb(255, 0, 255), 5, 5, 0, "FPS: %i", gameFPS);	//display FPS on screen
+
+			//BEGIN PROJECT RENDER================
+
+
+			//FLIP BUFFERS========================
+			al_flip_display();
+			al_clear_to_color(al_map_rgb(0,0,0));
+		}
+	}
+
+	//==============================================
+	//DESTROY PROJECT OBJECTS
+	//==============================================
+
+	//SHELL OBJECTS=================================
+	al_destroy_font(font18);
+	al_destroy_timer(timer);
+	al_destroy_event_queue(event_queue);
+	al_destroy_display(display);
+
+	return 0;
+}
