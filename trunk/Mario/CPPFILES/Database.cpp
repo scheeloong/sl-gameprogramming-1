1445c650f5928b6269a58cbb107cbbbb69169ac0
viv.liu25@gmail.com <viv.liu25@gmail.com@3fcf651d-5a50-4a49-f97f-91c100888062>
2012-07-11 20:16:40 +0000

    Replaced Database files with mine

--- trunk/Mario/CPPFILES/Database.cpp
+++ trunk/Mario/CPPFILES/Database.cpp
@@ -1,3 +1,50 @@
 #include "Database.h"
 
 // Defining Methods
+Database::Database()
+{
+	makePlayer(); // make a player
+	// enemies and powerups remain empty before tileblock sets it
+}
+		
+void Database::makePlayer()
+{
+	Player *pl = new Player(); 
+	players.push_back(pl);
+}
+
+void Database::makeEnemy()
+{
+	Enemy *en = new Enemy(); 
+	enemies.push_back(en); 
+}
+
+void Database::makePowerUps()
+{
+	PowerUp *pu = new PowerUp(); 
+	powerUps.push_back(pu); 
+}
+
+void destroyPlayer()
+{
+	for( iterP = players.begin(); iterP != players.end(); )
+	{
+		if(! (*iterP)->GetAlive())
+		{
+			delete (*iterP);
+			iter = objects.erase(iter);
+		}
+		else
+			iterP++;
+	}
+}
+
+void destroyEnemy()
+{
+
+}
+
+void destroyPowerUps()
+{
+
+}
