1f6da70330670a8f020ab96b917c76097fdb815b
viv.liu25@gmail.com <viv.liu25@gmail.com@3fcf651d-5a50-4a49-f97f-91c100888062>
2012-07-18 21:32:38 +0000

    Now with the two Goomba files. oops from rev. 56

---
+++ trunk/Mario/CPPFILES/HFILES/Goomba.h
@@ -0,0 +1,17 @@
+#ifndef GOOMBA_H
+#define GOOMBA_H
+
+#include "Classes.h"
+#include "HFILES/GameObject.h"
+#include "HFILES/Enemy.h"
+
+class Goomba : public Enemy
+{
+private:
+public:
+	Goomba() : Enemy () {}
+	void Init(int x, int y, int velX, int velY, int dirX, int dirY, ALLEGRO_BITMAP *image);
+	void draw();
+};
+
+#endif GOOMBA_H
